8a76b40dfdebb2d00a85b77b484379a96bfdc628
Isaac <igrande0@gmail.com>
2014-11-04 23:06:18 -0600

    changes to crawler; however, we will have to abandon the dotabuff crawler due to dotabuff's robots.txt

--- dota2/spiders/dbuff.py
+++ dota2/spiders/dbuff.py
@@ -6,20 +6,28 @@ class DmozSpider(scrapy.Spider):
     name = "dbuff"
     allowed_domains = ["dotabuff.com"]
     start_urls = [
-        "http://www.dotabuff.com/players/105496685/heroes/"
+        "http://www.dotabuff.com/players/105496685/heroes"
     ]
 
     def parse(self, response):
-        #filename = response.url.split("/")[-2]
-        #with open(filename, 'wb') as f:
-        #    f.write(response.body)
-		for sel in response.xpath('//ul/li'):
+		for row in response.xpath('//table/tbody/tr'):
 			item = Dota2Item()
-			item['name'] = sel.xpath('a/text()').extract()
-			item['hero'] = sel.xpath('text()').extract()		    
-			item['success'] = sel.xpath('a/@href').extract()
-			item['played'] = sel.xpath('text()').extract()
-			item['winrate'] = sel.xpath('text()').extract()
-			item['kda'] = sel.xpath('text()').extract()
-			print name, hero, success, played, winrate, kda
-			#yield item
+			item['name'] = '' # not sure what to do with this one right now
+			item['hero'] = ''
+			item['played'] = ''
+			item['winrate'] = ''
+			item['kda'] = ''
+			
+			for cell in row.xpath('//td'):
+				print cell.extract()
+				if(cell.xpath('/div[@class="subtext minor"]').extract() != ''):
+					item['hero'] = cell.xpath('a/text()').extract()	
+				elif(cell.xpath('/div/div[@class="segment segment-matches"]') != ''):
+					item['played'] = cell.xpath('text()').extract()
+				elif(cell.xpath('/div/div[@class="segment segment-win-ratio"]') != ''):
+					item['winrate'] = cell.xpath('text()').extract()
+				elif(cell.xpath('/div/div[@class="segment segment-kda-ratio"]') != ''):
+					item['kda'] = cell.xpath('text()').extract()
+				
+			print item['name'], item['hero'], item['played'], item['winrate'], item['kda']
+			yield item
