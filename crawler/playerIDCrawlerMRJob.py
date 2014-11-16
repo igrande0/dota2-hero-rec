d6b7e23418b580d358dc06f9bd6820f167cba34f
U-Isaac-PC\Isaac <igrande0@gmail.com>
2014-11-16 00:09:05 -0600

    fixes to MRjob script; added example MRjob output

--- crawler/playerIDCrawlerMRJob.py
+++ crawler/playerIDCrawlerMRJob.py
@@ -3,24 +3,27 @@
 """
 
 from __future__ import division
-
+import json
+import requests
 from mrjob.job import MRJob
+from math import sqrt
+
 
 STEAM_KEY = 'CCAFC265C13500E30C9E8BC6E04EEE9C'
 
 def success_rating(won, lost):
 # a success rating that considers win/loss ratio along with # of games return
 # normalized between 0 and 100%
-    played (((won + 1.9208) / (won + lost) \
-             - 1.96 * sqrt(((won * lost) / (won + lost)) + 0.9604) \
-                / (won + lost)) / (1 + 3.8416 / (won + lost)))*100
+    return (((won + 1.9208) / (won + lost) \
+            - 1.96 * sqrt(((won * lost) / (won + lost)) + 0.9604) \
+            / (won + lost)) / (1 + 3.8416 / (won + lost)))*100
 
 class ProfileCreator(MRJob):
 
-    def reducer_get_matches(self, _, user_id):
+    def mapper_get_matches(self, _, user_id):
     # for each user, yeild all matches: (user_id, match_id)
         history_url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/'
-        history_params = dict(key=STEAM_KEY, min_players=10, account_id=user_id+76561197960265728)
+        history_params = dict(key=STEAM_KEY, min_players=10, account_id=int(user_id)+76561197960265728)
 
         current_match = 1
         last_match_id = 0
@@ -36,7 +39,7 @@ class ProfileCreator(MRJob):
 
             for match in match_history['result']['matches']:
                 # the last match of the previous page is the first match of the current page
-                if 'start_at_match_id' in self.history_params and \
+                if 'start_at_match_id' in history_params and \
                    history_params['start_at_match_id'] == match['match_id']:
                     continue
 
@@ -53,18 +56,21 @@ class ProfileCreator(MRJob):
     # for each match, yeild hero details:
     # (user_id, {"hero_name:" {"matches": ,"won": , "kills":, "deaths": , "assists": }})
         details_url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/'
-        details_params = dict(key=STEAM_KEY, account_id=user_id+76561197960265728)
+        details_params = dict(key=STEAM_KEY, account_id=int(user_id)+76561197960265728, match_id=match_id)
 
-        player_profiles = {}
+        details_resp = requests.get(url=details_url, params=details_params)
+        match_details = json.loads(details_resp.text)
+
+        heroes = {}
 
         radiant_win = match_details['result']['radiant_win']
         for player in match_details['result']['players']:
-            if player['account_id'] == user_id:
+            if player['account_id'] == int(user_id):
                 if player['leaver_status'] != 0:
                     break
 
                 hero_id = player['hero_id']
-                heroes[hero_id] = {}
+                heroes[hero_id] = heroes.get(hero_id, {})
                 if (radiant_win == True and player['player_slot'] < 5) or \
                    (radiant_win == False and player['player_slot'] > 5):
                     heroes[hero_id]['won'] = 1
@@ -85,13 +91,14 @@ class ProfileCreator(MRJob):
     #           ...)
         all_heroes = {}
 
-        for hero_id, stats in heroes.iteritems():
-            all_heroes[hero_id] = all_heroes.getKey(hero_id, {})
-            all_heroes[hero_id]['matches'] = all_heroes[hero_id].getKey('matches', 0) + stats['matches']
-            all_heroes[hero_id]['won'] = all_heroes[hero_id].getKey('won', 0) + stats['won']
-            all_heroes[hero_id]['kills'] = all_heroes[hero_id].getKey('kills', 0) + stats['kills']
-            all_heroes[hero_id]['deaths'] = all_heroes[hero_id].getKey('deaths', 0) + stats['deaths']
-            all_heroes[hero_id]['assists'] = all_heroes[hero_id].getKey('assists', 0) + stats['assists']
+        for hero_item in heroes:
+            for hero_id, stats in hero_item.iteritems():
+                all_heroes[hero_id] = all_heroes.get(hero_id, {})
+                all_heroes[hero_id]['matches'] = all_heroes[hero_id].get('matches', 0) + stats['matches']
+                all_heroes[hero_id]['won'] = all_heroes[hero_id].get('won', 0) + stats['won']
+                all_heroes[hero_id]['kills'] = all_heroes[hero_id].get('kills', 0) + stats['kills']
+                all_heroes[hero_id]['deaths'] = all_heroes[hero_id].get('deaths', 0) + stats['deaths']
+                all_heroes[hero_id]['assists'] = all_heroes[hero_id].get('assists', 0) + stats['assists']
 
         yield user_id, all_heroes
 
@@ -103,21 +110,23 @@ class ProfileCreator(MRJob):
     #           ...)
         norm_heroes = {}
 
-        for hero_id, stats in heroes.iteritems():
-            norm_heroes[hero_id]['matches'] = stats['matches']
-            norm_heroes[hero_id]['won'] = stats['won']
-            norm_heroes[hero_id]['win_perc'] = stats['won'] / stats['matches']
-            if stats['deaths'] != 0:
-                norm_heroes[hero_id]['kda'] = (stats['kills'] + stats['assists']) / stats['deaths']
-            else:
-                norm_heroes[hero_id]['kda'] = (stats['kills'] + stats['assists'])
-            norm_heroes[hero_id]['success'] = success_rating(stats['won'], stats['matches'] - stats['won'])
+        for hero_item in heroes:
+            for hero_id, stats in hero_item.iteritems():
+                norm_heroes[hero_id] = norm_heroes.get(hero_id, {})
+                norm_heroes[hero_id]['matches'] = stats['matches']
+                norm_heroes[hero_id]['won'] = stats['won']
+                norm_heroes[hero_id]['win_perc'] = stats['won'] / stats['matches']
+                if stats['deaths'] != 0:
+                    norm_heroes[hero_id]['kda'] = (stats['kills'] + stats['assists']) / stats['deaths']
+                else:
+                    norm_heroes[hero_id]['kda'] = (stats['kills'] + stats['assists'])
+                norm_heroes[hero_id]['success'] = success_rating(stats['won'], stats['matches'] - stats['won'])
         
         yield user_id, norm_heroes
 
     def steps(self):
         return [
-            self.mr(reducer=self.reducer_get_matches),
+            self.mr(mapper=self.mapper_get_matches),
             self.mr(mapper=self.mapper_get_match_details,
                     reducer=self.reducer_compile_hero_stats),
             self.mr(reducer=self.reducer_normalize_hero_stats)
